45cc95777e8925a169a415732b325a6bc53003d9
Casey Williams <cjw19@ic.ac.uk>
2022-02-12 13:35:59 +0000

    finish ex1

--- lab3/lib/peer.ex
+++ lab3/lib/peer.ex
@@ -12,7 +12,7 @@ defp next(my_id, peer_list) do
     { :broadcast, max_broadcasts, timeout } ->
       # Create a list of empty counts for each peer (including self)
       # that represent the number of messages broadcast and sent
-      messages = for peer <- peer_list, do: {0, 0}
+      messages = for _ <- peer_list, do: {0, 0}
       broadcast(my_id, peer_list, max_broadcasts, timeout, messages)
   end
 end # next
@@ -20,37 +20,36 @@ end # next
 defp broadcast(my_id, peer_list, max_broadcasts, timeout, messages) do
   if max_broadcasts > 0 do
     new_delivered_messages = deliver_to_peers(my_id, peer_list, messages)
-    # TODO: Continue from here
     receive do
-      { :deliver, peer, peer_id } ->
-        new_received_messages = receive_from_peer(new_delivered_messages, peer_id)
+      { :deliver, _peer, peer_id } ->
+        new_messages = receive_from_peer(new_delivered_messages, peer_id)
+        broadcast(my_id, peer_list, max_broadcasts - 1, timeout, new_messages)
+    after 
+      timeout -> print_messages(my_id, messages)
     end
-    broadcast(my_id, peer_list, max_broadcasts - 1, timeout, new_messages)
+  else
+    receive do
+      { :deliver, _peer, peer_id } ->
+        new_messages = receive_from_peer(messages, peer_id)
+        broadcast(my_id, peer_list, max_broadcasts, timeout, new_messages)
+    after 
+      timeout -> print_messages(my_id, messages)
+    end
   end
 end # broadcast
 
+defp print_messages(my_id, messages) do
+  IO.puts("Peer#{my_id}: " <> Enum.map_join(messages, " ", &(inspect(&1))))
+end # print_messages
+
 defp deliver_to_peers(my_id, peer_list, messages) do
-  if peer_list != [] do
-    [peer | peer_rest] = peer_list
-    [message_count | message_rest] = messages
-    send peer, { :deliver, self(), my_id }
-    {sent, received} = message_count
-    updated_count = {sent + 1, received}
-    [updated_count | deliver_to_peers(my_id, peer_rest, message_rest)]
-  else
-    messages
-  end
+  Enum.each(peer_list, fn id -> send id, { :deliver, self(), my_id } end)
+  Enum.map(messages, fn {s, r} -> {s+1, r} end)
 end # deliver_to_peers
 
 defp receive_from_peer(messages, peer_id) do
-  [message_count | message_rest] = messages
-  if peer_id == 0 do
-    {sent, received} = message_count
-    updated_count = {sent, received + 1}
-    [updated_count | message_rest]
-  else
-    [message_count | receive_from_peer(message_rest, peer_id - 1)]
-  end
+  {sent, received} = Enum.at(messages, peer_id)
+  List.replace_at(messages, peer_id, {sent, received + 1})
 end # receive_from_peer
 
 end # Peer
